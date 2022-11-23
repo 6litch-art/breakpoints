61b1268133b3e9a0bd9458a27eb376299f332310
Marco Meyer (www) <marco.meyerconde@gmail.com>
2022-11-23 08:39:58 +0100

    Update package

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var debug = debug || true;
+var getBreakpoint = function () {
+	return window.getComputedStyle(document.body, ":before").content.replace(/\"/g, '');
+}
+
+var breakpoint = getBreakpoint();
+if(debug) {
+	console.log("Breakpoint: " + breakpoint);
+}
+
+window.addEventListener('resize', function() {
+	breakpoint = getBreakpoint();
+	if(debug) console.log("Breakpoint: " + breakpoint);
+}, false);
