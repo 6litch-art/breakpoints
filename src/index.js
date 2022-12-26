730dffcdbecc2a42288983b7bba1c5c0ef8b475c
Server LTO <www-data@latoucheoriginale.fr>
2022-12-26 00:51:57 +0000

    Update, cleanup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import './scss/index.scss';
 
-var debug = debug || true;
 var getBreakpoint = function () {
 	return window.getComputedStyle(document.body, ":before").content.replace(/\"/g, '');
 }
 
 var breakpoint = getBreakpoint();
-if(debug) {
-	console.log("Breakpoint: " + breakpoint);
-}
+console.log("Breakpoint: " + breakpoint);
 
 window.addEventListener('resize', function() {
 	breakpoint = getBreakpoint();
-	if(debug) console.log("Breakpoint: " + breakpoint);
+	console.log("Breakpoint: " + breakpoint);
 }, false);
 
 export default breakpoint;
