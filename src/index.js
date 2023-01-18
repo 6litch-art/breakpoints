18aa4500343db67f8968112351b4e43f7756af3b
Marco Meyer (www) <marco.meyerconde@gmail.com>
2023-01-18 02:12:22 +0100

    Update, cleanup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import './scss/index.scss';
 
+var debug = debug || true;
 var getBreakpoint = function () {
 	return window.getComputedStyle(document.body, ":before").content.replace(/\"/g, '');
 }
 
 var breakpoint = getBreakpoint();
-console.log("Breakpoint: " + breakpoint);
+if(debug) {
+	console.log("Breakpoint: " + breakpoint);
+}
 
 window.addEventListener('resize', function() {
 	breakpoint = getBreakpoint();
-	console.log("Breakpoint: " + breakpoint);
+	if(debug) console.log("Breakpoint: " + breakpoint);
 }, false);
 
 export default breakpoint;
