256936230e63224500b7562c1d819c7d8325d8ca
Marco Meyer <marco.meyer@glitchr.io>
2023-02-11 18:25:33 +0900

    Update, cleanup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import './scss/index.scss';
-
 var debug = debug || true;
 var getBreakpoint = function () {
 	return window.getComputedStyle(document.body, ":before").content.replace(/\"/g, '');
 }
 
-var breakpoint = getBreakpoint();
+var breakpoint = window.breakpoint = getBreakpoint();
 if(debug) console.log("Breakpoint: " + breakpoint);
 
 window.addEventListener('resize', function() {
